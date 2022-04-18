01c6fc4e2d16003b8cb67e7734fa00eae5871123
odoyonina <odoyoninah@gmail.com>
2022-04-18 19:41:08 +0300

    import search results

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
 
 @Component({
   selector: 'app-github',
@@ -6,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
-
-  constructor() { }
+results!: any;
+  constructor(public searchService: GithubService) { }
 
   ngOnInit(): void {
   }
 searchGithub(searchTerm: string) {
-  
+  this.searchService.getUser(searchTerm).subscribe({
+    next: (data: any) => {
+      this.results = data;
+      console.log(this.results);
+    }
+  });
+
 }
 }
