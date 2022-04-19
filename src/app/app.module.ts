b178a408680147ee7566eeea5f3f599c609e3aa0
odoyonina <odoyoninah@gmail.com>
2022-04-19 18:02:48 +0300

    add nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
